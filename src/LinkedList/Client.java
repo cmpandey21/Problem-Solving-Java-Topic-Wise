71db2457ded99ba3233a942acee731a436a1d392
Chandra Mohan Pandey <pandeymohan360@gmail.com>
2023-08-10 12:54:04 +0530

    updated programs 10-Aug-2023

--- src/LinkedList/Client.java
+++ src/LinkedList/Client.java
@@ -7,15 +7,21 @@ public class Client {
 	public static void main(String[] args) throws Exception {
 	
 		LinkedList list=new LinkedList();
-		/*list.addFirst(10);
+		list.addFirst(10);
 		list.addLast(20);
 		list.addLast(30);
-		list.addFirst(40);*/
-		//list.addFirst(50);
-		//list.getFirst();
-		list.getLast();
+		list.addFirst(40);
+		list.addFirst(50);
+	    list.display();
+		System.out.println(list.getAt(1));
+		System.out.println(list.getFirst());
+		System.out.println(list.getLast());
+		System.out.println(list.removeFirst());
 		list.display();
-		
+		System.out.println(list.removeLast());
+		list.display();
+		System.out.println(list.removeAt(1));
+		list.display();
 	}
 
 }
